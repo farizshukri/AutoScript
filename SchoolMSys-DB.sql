9cee7e373f705fb5bb259c64d1b6d74d2188588a
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2024-04-10 18:20:30 +0800

    initial commit

--- SchoolMSys-DB.sql
+++ SchoolMSys-DB.sql
@@ -5,3 +5,11 @@ CREATE TABLE Students (
     DateOfBirth DATE,
     Address TEXT
 );
+
+CREATE TABLE Teachers (
+    TeacherID AUTOINCREMENT PRIMARY KEY,
+    Name TEXT,
+    Subject TEXT,
+    DateOfBirth DATE,
+    Address TEXT
+);
